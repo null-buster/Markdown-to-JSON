aa3ef6123b4c9cdbf0959c6cae92c47e2e280ebf
null-buster <rahul.8981513581@gmail.com>
2021-07-03 13:14:08 +0530

    initial-prototype complete

--- convert_md.py
+++ convert_md.py
@@ -1,7 +1,6 @@
 import argparse
 import os
 import json
-import re
 from uuid import uuid4
 from re import search
 #added script arguments
@@ -22,11 +21,7 @@ with open(md_file, 'r') as f:
 #print(text)
 
 lines = text.strip().split("\n")
-"""
-for i in range(len(lines)):
-    print("Printing line {}".format(i))
-    print(lines[i])
-"""
+
 output_json = []
 line_fields = ["question", "topic", "choices", "id", "complexity"]
 answer_fields = ["content", "isCorrect", "id"]
@@ -37,25 +32,31 @@ for line in lines:
     if "|" not in line:
         line_dict[line_fields[0]] = values[1]
         line_dict[line_fields[1]] = str(topic)
-        line_dict[line_fields[3]] = str(uuid4)
+        line_dict[line_fields[3]] = str(uuid4())
+        output_json.append(line_dict)
     else:
         answer_list = []
-        answer_dict = dict()
         answers = values[1].strip().split("|")
         print(answers)
         for ans in answers:
+            answer_dict = dict()
+            print(ans)
             isCorrect = False
             if search("yes", ans):
                 isCorrect = True
                 ans = ans.replace("yes", "", 1)
-            answer_dict[answer_fields[0]] = ans
+                answer_dict[answer_fields[0]] = ans.strip()
+            else:
+                answer_dict[answer_fields[0]] = ans.strip()
             answer_dict[answer_fields[1]] = isCorrect
             answer_dict[answer_fields[2]] = str(uuid4())
             answer_list.append(answer_dict)
 
         line_dict[line_fields[2]] = answer_list
-
-    output_json.append(line_dict)
+        temp = output_json[-1]
+        temp.update(line_dict)
+        output_json[-1] = temp
+    
     
 with open(out_file,'w') as f:
     json.dump(output_json,f,indent=4)
