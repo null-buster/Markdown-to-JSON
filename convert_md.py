c644c3e4cd7a232a5985d509b406474952a19804
null-buster <rahul.8981513581@gmail.com>
2021-07-03 14:20:28 +0530

    imporoved parsing by adding failsafe for blank lines

--- convert_md.py
+++ convert_md.py
@@ -27,35 +27,38 @@ line_fields = ["question", "topic", "choices", "id", "complexity"]
 answer_fields = ["content", "isCorrect", "id"]
 
 for line in lines:
-    line_dict = dict()
-    values = line.strip().split(":")
-    if "|" not in line:
-        line_dict[line_fields[0]] = values[1]
-        line_dict[line_fields[1]] = str(topic)
-        line_dict[line_fields[3]] = str(uuid4())
-        output_json.append(line_dict)
+    if line == "\n" or line == "":
+        continue
     else:
-        answer_list = []
-        answers = values[1].strip().split("|")
-        # print(answers)
-        for ans in answers:
-            answer_dict = dict()
-            # print(ans)
-            isCorrect = False
-            if search("yes", ans):
-                isCorrect = True
-                ans = ans.replace("yes", "", 1)
-                answer_dict[answer_fields[0]] = ans.strip()
-            else:
-                answer_dict[answer_fields[0]] = ans.strip()
-            answer_dict[answer_fields[1]] = isCorrect
-            answer_dict[answer_fields[2]] = str(uuid4())
-            answer_list.append(answer_dict)
+        line_dict = dict()
+        values = line.strip().split(":")
+        if "|" not in line:
+            line_dict[line_fields[0]] = values[1]
+            line_dict[line_fields[1]] = str(topic)
+            line_dict[line_fields[3]] = str(uuid4())
+            output_json.append(line_dict)
+        else:
+            answer_list = []
+            answers = values[1].strip().split("|")
+            # print(answers)
+            for ans in answers:
+                answer_dict = dict()
+                # print(ans)
+                isCorrect = False
+                if search("yes", ans):
+                    isCorrect = True
+                    ans = ans.replace("yes", "", 1)
+                    answer_dict[answer_fields[0]] = ans.strip()
+                else:
+                    answer_dict[answer_fields[0]] = ans.strip()
+                answer_dict[answer_fields[1]] = isCorrect
+                answer_dict[answer_fields[2]] = str(uuid4())
+                answer_list.append(answer_dict)
 
-        line_dict[line_fields[2]] = answer_list
-        temp = output_json[-1]
-        temp.update(line_dict)
-        output_json[-1] = temp
+            line_dict[line_fields[2]] = answer_list
+            temp = output_json[-1]
+            temp.update(line_dict)
+            output_json[-1] = temp
     
     
 with open(out_file,'w') as f:
